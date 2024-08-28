55e04129b124c632ed6bfcaa8a2de151a42b23c7
RyanMontville <ryan.a.montville@gmail.com>
2024-08-28 10:40:20 -0400

    adding event id to csv files

--- src/app/csv.service.ts
+++ src/app/csv.service.ts
@@ -8,9 +8,17 @@ export class CsvService {
 
   constructor(private http: HttpClient) { }
   getActualTotals() {
-    return this.http.get(`https://raw.githubusercontent.com/RyanMontville/actual-olympic-medals-ranking/main/actual_totals.csv`, { responseType: 'text' });
+    //return this.http.get(`https://raw.githubusercontent.com/RyanMontville/actual-olympic-medals-ranking/main/actual_totals.csv`, { responseType: 'text' });
+    return this.http.get(`https://raw.githubusercontent.com/RyanMontville/actual-olympic-medals-ranking/main/data/actual_totals.csv`, { responseType: 'text' });
   }
   getAllMedals() {
-    return this.http.get('https://raw.githubusercontent.com/RyanMontville/actual-olympic-medals-ranking/main/allmedals.csv', { responseType: 'text' });
+    //return this.http.get('https://raw.githubusercontent.com/RyanMontville/actual-olympic-medals-ranking/main/allmedals.csv', { responseType: 'text' });
+    return this.http.get('https://raw.githubusercontent.com/RyanMontville/actual-olympic-medals-ranking/main/data/team_and_individual_medals.csv', { responseType: 'text' });
+  }
+  getAthleteList() {
+    return this.http.get('', { responseType: 'text' });
+  }
+  getMedalsForEveryAthlete() {
+    return this.http.get('https://raw.githubusercontent.com/RyanMontville/actual-olympic-medals-ranking/main/data/medals_for_every_athlete.csv', { responseType: 'text' });
   }
 }
